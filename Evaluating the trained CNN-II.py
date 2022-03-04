81f6bfb09d9d4f1fa4968bfc78af43de67042062
fanweiying <62782786+fanweiying@users.noreply.github.com>
2022-03-04 09:49:37 +0800

    Add files via upload

---
+++ Evaluating the trained CNN-II.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 27 22:09:50 2020
+
+@author: wyf
+"""
+#Test CNN-II
+from __future__ import absolute_import, division, print_function, unicode_literals
+import random
+import pathlib
+import tensorflow as tf
+import time
+time_start=time.time()
+
+
+model=tf.keras.models.load_model(r'./Trained_CNN-II.h5')
+#model=tf.keras.models.load_model(r'./2D_symmetry_calssfication/Results of CNN-II/Adam/model/CNN-II-Adam-001.h5')
+test_dir = pathlib.Path(r'./2D_symmetry_calssfication/Dataset 2./test')
+test_image_paths=list(test_dir.glob('*/*'))
+#%%
+test_image_paths=[str(path) for path in test_image_paths]  
+random.shuffle(test_image_paths)  
+# test_image_paths[:5]      
+test_label_names= sorted(item.name for item in test_dir.glob('*/') if item.is_dir()) 
+print("Label：")
+print(test_label_names)
+test_label_to_index =dict((name, index) for index, name in enumerate(test_label_names))
+print("The index corresponding to the label：")
+print(test_label_to_index)
+test_labels=[test_label_to_index[pathlib.Path(path).parent.name] for path in test_image_paths]
+test_image_count=len(test_image_paths)
+print('num_test')
+print(test_image_count)
+
+def load_and_preprocess_from_path(path):
+    image = tf.io.read_file(path)  
+    image = tf.image.decode_jpeg(image, channels=1)   
+    image = tf.image.resize(image, [200, 200])  
+    image /= 255.0  
+    return image
+
+
+#%%
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+test_image_paths_ds = tf.data.Dataset.from_tensor_slices(test_image_paths) 
+X_test_ds=test_image_paths_ds.map(load_and_preprocess_from_path, num_parallel_calls=AUTOTUNE)
+Y_test_ds = tf.data.Dataset.from_tensor_slices(tf.cast(test_labels, tf.int64))
+
+
+#%%
+ds = X_test_ds.repeat()
+ds = ds.batch(batch_size=1)
+X_test = ds.prefetch(buffer_size=AUTOTUNE)
+ds1 = Y_test_ds.repeat()
+ds1 = ds1.batch(batch_size=1)
+Y_test = ds1.prefetch(buffer_size=AUTOTUNE)
+dataset=tf.data.Dataset.zip((X_test,Y_test))
+
+
+#%%
+Y_predict_origin=model.predict(X_test,steps=test_image_count/1)
+#Convert predicted probability values to integer labels 
+print('The time of identifying an image：%fs!'%((time.time() - time_start)/test_image_count))
+
+
+#%%
+score=model.evaluate(dataset,steps=test_image_count/1,verbose=2)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+
+import numpy as np
+Y_predict=[]
+for i in range(len(Y_predict_origin)):
+    Y_predict.append(np.argmax(Y_predict_origin[i]))
+print('Y_predict:')
+print(Y_predict)
+
+#%%
+print('num_test')
+print(len(Y_predict))
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import itertools
+import matplotlib.pyplot as plt
+#Plot the confusion matrix for the test set 
+#The parameter y_true is the true label of the test data set, and y_pred is the prediction result of the network on the test data set 
+
+# 绘制混淆矩阵
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues,save_flg = True):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    Input
+    - cm : 计算出的混淆矩阵的值
+    - classes : 混淆矩阵中每一行每一列对应的列
+    - normalize : True:显示百分比, False:显示个数
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    print(cm)
+    plt.figure(figsize=(4.5,3.5))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+#    font = {'family': 'serif',
+#        'serif': 'Times New Roman',
+#        'weight': 'normal',
+#        'size': 10}
+#    plt.rc('font', **font)
+    #以下两行设置全局字体，在本例中，坐标轴刻度和图例均用新罗马字体来表示
+   
+    
+    plt.rc('font',family='Times New Roman')
+    plt.rcParams.update({'font.size': 7.5})
+
+    #plt.title(title)
+    plt.colorbar()
+    font={'family' : 'Times New Roman','weight' : 'normal'}
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks,classes,fontsize=10,fontproperties = 'Times New Roman')
+    plt.yticks(tick_marks,classes,fontsize=10,fontproperties = 'Times New Roman')
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if cm[i, j]>=0.02:
+            plt.text(j, i, format(cm[i, j], fmt),
+                     horizontalalignment="center",
+                     verticalalignment="center",      ##需要减小一下字宽
+                     color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label',font,fontsize=10)
+    plt.xlabel('Predicted label',font,fontsize=10)
+    
+    
+    plt.tight_layout()
+    if save_flg:
+        plt.savefig("./2D_symmetry_calssfication/Results of CNN-II/confusion_matrix_CNN-II.pdf") 
+    plt.show()
+    
+    
+m=len(Y_predict)
+Y_true=[]
+for i in range (m):
+    Y_true.append(test_labels[i])
+
+import pandas as pd
+cm = confusion_matrix(Y_true,Y_predict, labels=range(len(test_label_names)))
+
+plot_confusion_matrix(cm,range(len(test_label_names)),normalize=True, title='confusion matrix')
+#call confusion matrix 
+
+
+#%%
+cm = confusion_matrix(Y_true,Y_predict, labels=range(len(test_label_names)))
+pdcm=pd.DataFrame(cm)
+print(pdcm)
+
